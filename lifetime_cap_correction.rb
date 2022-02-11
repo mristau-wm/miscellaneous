fa1682d5115df45552c0572810661bdf39309bff
Michael Ristau <82000708+mristau-wm@users.noreply.github.com>
2022-02-11 15:19:53 -0800

    Update lifetime_cap_correction.rb

--- lifetime_cap_correction.rb
+++ lifetime_cap_correction.rb
@@ -131,3 +131,170 @@ Advertising::Flight.find_each do |flight|
 rescue => e
   puts "Exception occurred: #{e.class} #{e.message} #{e.backtrace.join("\n")}"
 end
+
+
+
+###############################
+# Correcting single flight    #
+###############################
+
+def sync_balances(flight, dry_run: true)
+  api = Adzerk::Api.new
+
+  campaign = flight.campaign
+  organization = campaign.listing.advertising_organization
+  initial_lifetime_cap = flight.lifetime_cap
+
+  puts "Processing flight #{flight.id} #{'(dry run)' if dry_run}"
+
+  # Determine if the flight needs correction
+  accurate_lifetime_cap_cents = (flight.revenue_from_deltas.to_i + flight.balance)
+  accurate_lifetime_cap = accurate_lifetime_cap_cents / 100.0
+
+  transfer_args = {
+    price_value: 0,
+    organization_id: organization.id,
+    debit_entity: organization,
+    credit_entity: flight,
+    user_id: nil
+  }
+  transfer_service = Advertising::TransferService::Cash.new(transfer_args)
+
+  budget = flight.accounts.sum { |account| transfer_service.send(:adjusted_campaign_budget, account) }
+  current_lifetime_cap = budget / 100.0
+
+  puts "Current lifetime cap: \$#{current_lifetime_cap}. Accurate lifetime cap: \$#{accurate_lifetime_cap}"
+
+  if current_lifetime_cap == accurate_lifetime_cap
+    puts "Skipping flight. Lifetime cap does not need correction."
+    next
+  end
+
+  # Determine the correction amount
+  adjustment_amount = (current_lifetime_cap - accurate_lifetime_cap).abs * 100 # convert dollars to cents
+
+  # Apply the correction for that amount
+  if current_lifetime_cap > accurate_lifetime_cap
+    puts "Subtracting \$#{adjustment_amount} from lifetime cap"
+    adjustment_type = 'decrease'
+
+    unless dry_run
+      Plutus::Entry.create!(
+        description: 'advertising.lifetime_cap_adjustment.credit_exclusion',
+        debits: [{ account: Advertising::Accounts.cash_correction, amount: adjustment_amount }],
+        credits: [{ account: flight.cash_account, amount: adjustment_amount }]
+      );nil
+
+      Plutus::Entry.create!(
+        description: 'One-time Kevel lifetime cap correction entry',
+        debits: [{ account: flight.cash_account, amount: adjustment_amount }],
+        credits: [{ account: Advertising::Accounts.cash_correction, amount: adjustment_amount }]
+      );nil
+    end
+  else
+    puts "Adding \$#{adjustment_amount} to lifetime cap"
+    adjustment_type = 'increase'
+
+    unless dry_run
+      Plutus::Entry.create!(
+        description: 'One-time Kevel lifetime cap correction entry',
+        debits: [{ account: Advertising::Accounts.cash_correction, amount: adjustment_amount }],
+        credits: [{ account: flight.cash_account, amount: adjustment_amount }]
+      );nil
+
+      Plutus::Entry.create!(
+        description: 'advertising.lifetime_cap_adjustment.debit_exclusion',
+        debits: [{ account: flight.cash_account, amount: adjustment_amount }],
+        credits: [{ account: Advertising::Accounts.cash_correction, amount: adjustment_amount }]
+      );nil
+    end
+  end
+
+  # Make whole dollar correction
+  rounded_up_accurate_lifetime_cap_cents = accurate_lifetime_cap_cents.ceil(-2)
+  whole_dollar_correction_cents = rounded_up_accurate_lifetime_cap_cents - accurate_lifetime_cap_cents
+  rounded_up_accurate_lifetime_cap = rounded_up_accurate_lifetime_cap_cents / 100.0
+  
+  if whole_dollar_correction_cents.zero?
+    puts "Does not need whole dollar correction"
+  else
+    puts "Creating whole dollar correction entry of #{whole_dollar_correction_cents} cents"
+
+    unless dry_run
+      Plutus::Entry.create!(
+        description: 'advertising.budget_transfer.whole_dollar_correction',
+        debits: [{ account: Advertising::Accounts.cash_correction, amount: whole_dollar_correction_cents }],
+        credits: [{ account: flight.cash_account, amount: whole_dollar_correction_cents }],
+      )
+    end
+  end
+
+  flight.reload
+  flight.cash_account.reload
+
+  unless dry_run
+    api.update_flight_cap(flight_id: flight.adzerk_id, amount: rounded_up_accurate_lifetime_cap)
+  end
+  
+  csv_summary = "#{organization.id},#{flight.id},#{flight.adzerk_id},#{adjustment_amount},#{adjustment_type},#{current_lifetime_cap},#{accurate_lifetime_cap},#{rounded_up_accurate_lifetime_cap},#{initial_lifetime_cap},#{flight.balance},#{flight.revenue_from_deltas},#{whole_dollar_correction_cents}"
+
+  unless dry_run
+    sleep 1 # allow for db replication time
+    flight.reload
+    final_core_balance = flight.balance
+    final_kevel_lifetime_cap = flight.lifetime_cap
+    final_kevel_balance = final_kevel_lifetime_cap - flight.revenue_from_deltas
+
+    flight.accounts { |a| a.reload }
+    transfer_args = {
+      price_value: 0,
+      organization_id: organization.id,
+      debit_entity: organization,
+      credit_entity: flight,
+      user_id: nil
+    }
+    transfer_service = Advertising::TransferService::Cash.new(transfer_args)
+
+    final_budget = flight.accounts.sum { |account| transfer_service.send(:adjusted_campaign_budget, account) }
+
+    csv_summary += ",#{final_core_balance},#{final_kevel_lifetime_cap},#{final_kevel_balance},#{final_budget}"
+  end
+
+  puts "CSV summary: #{csv_summary}"
+rescue => e
+  puts "Exception occurred: #{e.class} #{e.message} #{e.backtrace.join("\n")}"
+end
+
+
+
+
+
+
+
+###############################
+# Checking single flight data #
+###############################
+
+# flight_id = 981
+
+# flight = Advertising::Flight.find flight_id
+# campaign = flight.campaign
+# organization = campaign.listing.advertising_organization
+
+# transfer_args = {
+#   price_value: 0,
+#   organization_id: organization.id,
+#   debit_entity: organization,
+#   credit_entity: flight,
+#   user_id: nil
+# }
+# transfer_service = Advertising::TransferService::Cash.new(transfer_args)
+
+# budget = flight.accounts.sum { |account| transfer_service.send(:adjusted_campaign_budget, account) }.to_i
+# # 3000
+
+# flight.lifetime_cap # 3000
+# flight.balance #  2994
+# revenue = flight.revenue_from_deltas.to_i # 101
+# flight.lifetime_cap - revenue # 2899
+
