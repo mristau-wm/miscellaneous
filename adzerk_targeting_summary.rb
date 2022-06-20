f7b02276de551f4dc9b9fa054e1264a49a4a3963
Michael Ristau <82000708+mristau-wm@users.noreply.github.com>
2022-06-20 16:08:41 -0700

    Create adzerk_targeting_summary.rb

---
+++ adzerk_targeting_summary.rb
@@ -0,0 +1,25 @@
+# Run in console
+# Prints CSV to summarize flight targeting
+
+api = Adzerk::Api.new
+advertisers = api.all_advertisers
+
+advertisers.each do |advertiser|
+  campaigns = api.advertiser_campaigns(advertiser_id: advertiser.id)[:items]
+
+  campaigns.each do |campaign|
+    adzerk_campaign = Adzerk::Campaign.new campaign
+
+    flights = api.campaign_flights(campaign_id: adzerk_campaign.id)
+
+    flights.each do |flight|
+      sanitized_keywords = ''
+
+      if flight.keywords
+        sanitized_keywords = flight.keywords.gsub("\n", '\n').gsub(' ', '')
+      end
+
+      puts "\"#{flight.id}\",\"#{flight.site_zone_targeting}\",\"#{flight.custom_targeting}\",\"#{sanitized_keywords}\""
+    end
+  end
+end
