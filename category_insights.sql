fa9ef56e0db45b35814ff34c5836b0d7a220563c
Michael Ristau <82000708+mristau-wm@users.noreply.github.com>
2022-08-29 11:26:12 -0400

    Update category_insights.sql

--- category_insights.sql
+++ category_insights.sql
@@ -1,3 +1,41 @@
+WITH
+
+category_insights_1 AS (
+  select date_key
+    , advertiser_id
+    , flight_id
+    , i.user_id
+    , arr.position as "category_position"
+    , arr.item_object as "category_data"
+  from advertising_user_insights_90_days i
+  join user_affinities a on (i.user_id = a.user_id)
+  , jsonb_array_elements(a.categories) with ordinality arr(item_object, position)
+  where a.categories <> '{}' and a.categories <> '[]'
+),
+
+category_insights_2 AS (
+select advertiser_id
+  , (category_data->>'value')::int "category_id"
+  , count(user_id) "user_impressions"
+from category_insights_1
+group by advertiser_id, category_id
+order by advertiser_id, category_id
+)
+
+select advertiser_id
+  , category_id
+  , user_impressions
+from category_insights_2
+where category_id in (2,3,4,5) -- Flower, Concentrates, Vape Pens, Edibles
+;
+
+-- advertiser_id	category_id	user_impressions
+-- 410	2	23
+-- 410	3	9
+-- 410	4	6
+
+-------------------------
+
 WITH category_insights AS (
   select date_key
     , advertiser_id
